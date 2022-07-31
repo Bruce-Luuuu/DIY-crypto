e54c05521fc5f0192f9355e4d9fbe02b2c8e3f31
Bruce_Lu <luyifu@126.com>
2022-07-31 14:04:29 +0800

    finished sm4 code

--- SM4/SM4_optimized/main.cpp
+++ SM4/SM4_optimized/main.cpp
@@ -5,7 +5,10 @@
 #include <random>
 #include <ctime>
 #include <string>
+#include <vector>
 using namespace std;
+
+#define Bytes_per_Block 16
 
 char CHAR_SET[63] = {
     '0','1','2','3','4','5','6','7','8','9',
@@ -17,7 +20,7 @@ char CHAR_SET[63] = {
     'y','z','\0'
 };
 
-string GenMsg(string filename, uint32_t len) {
+string GenStr(uint32_t len) {
     static mt19937 rnd(time(0));
     static uniform_int_distribution<> range(0, 61);
     string res = "";
@@ -27,24 +30,48 @@ string GenMsg(string filename, uint32_t len) {
     return res;
 }
 
+void test(int block_num) {
+    uint8_t* key = (uint8_t*)malloc(16*sizeof(uint8_t));
+    uint8_t* plaintext = (uint8_t*)malloc(block_num * Bytes_per_Block * sizeof(uint8_t));
+
+    uint8_t* ciphertext = plaintext; // 重用变量以节约空间
+    memcpy(key, GenStr(16).c_str(), 16);
+    memcpy(plaintext, GenStr(block_num * Bytes_per_Block).c_str(), block_num * Bytes_per_Block);
+
+    SM4_Key sm4_key = (uint32_t*)malloc(32 * sizeof(uint32_t));
+    if (sm4_key == NULL) {
+        cout << "malloc failed.\n";
+        return;
+    }
+
+    SM4_KeyInit(key, sm4_key);
+    uint32_t time = 0;
+    clock_t start, end, whole = 0;
+    while (true) {
+        start = clock();
+        for (int i = 0; i < block_num / 8; i++) {
+            SM4_Encrypt((plaintext + 8 * Bytes_per_Block * i), (ciphertext + 8 * Bytes_per_Block * i), sm4_key);
+        }
+        end = clock();
+        whole += (end - start);
+        if (double(whole) / CLOCKS_PER_SEC > 3.0) break;
+        time++;
+    }
+    cout << "Doing SM4 encryption for " << time << " times on " << block_num << " message blocks: ";
+    cout << "Time cost is " << double(whole) / CLOCKS_PER_SEC << "s\n";
+    //SM4_Decrypt(ciphertext, plaintext, sm4_key);
+    SM4_KeyDelete(sm4_key);
+}
+
 
 int main() {
     if (SelfTest() == false) {
         cout << "SM4 implementation is not correct\n";
         exit(0);
     }
-    unsigned char key[16 * 8] = { 0 };
-    unsigned char in[16 * 8] = { 0 };
-    SM4_Key sm4_key = (uint32_t*)malloc(32 * sizeof(uint32_t));
-    // malloc 申请空间失败
-    if (sm4_key == NULL) {
-        cout << "malloc failed.\n";
+    vector<int> test_list = { 16, 64, 256, 1024, 8192, 16384 };
+    for (auto block_num : test_list) {
+        test(block_num);
     }
-
-    SM4_KeyInit(key, sm4_key);
-    SM4_Encrypt(in, in, sm4_key);
-    SM4_Decrypt(in, in, sm4_key);
-    SM4_KeyDelete(sm4_key);
-
     return 0;
 }
